82886546b78c5898824d387586c98bd25d594705
cyprian <cnwachukwu5@gmail.com>
2019-06-05 00:01:26 -0400

    Add config files

---
+++ src/main/java/com/cypriannwachukwu/h_scompany/appconfiguration/HSCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.cypriannwachukwu.h_scompany.appconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HSCompanyApplication.class, args);
+    }
+
+}
